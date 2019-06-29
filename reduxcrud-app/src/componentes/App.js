46f0e2f4f2e4dc59b80d1e834f4a072bb31852ba
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-06-29 18:55:03 -0500

    Inicio de la aplicación

---
+++ reduxcrud-app/src/componentes/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Imagen from './Imagen';
+import './../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/* ***** Componente: Imagen ***** */}
+    <Imagen></Imagen>
+    </div>
+  );
+}
+
+export default App;
