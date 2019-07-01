e557a2e30f844e9dbf81a6d4e44801b559d84712
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-07-01 07:22:57 -0500

    Creando Componente Header

--- reduxcrud-app/src/componentes/App.js
+++ reduxcrud-app/src/componentes/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Header from './Header';
 import Imagen from './Imagen';
 import './../css/App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './../store/store';
 
 function App() {
+
   return (
-    <div className="App">
-    {/* ***** Componente: Imagen ***** */}
-    <Imagen></Imagen>
-    </div>
+    <Provider store={store}>
+      <React.Fragment>
+        <div className="App">
+          {/* ***** Componente: Header ***** */}
+          <Header></Header>
+          {/* ***** Componente: Imagen ***** */}
+          <Imagen></Imagen>
+        </div>
+      </React.Fragment>
+    </Provider>
   );
 }
 
